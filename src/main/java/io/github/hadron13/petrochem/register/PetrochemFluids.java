f4a382b18c56e9c5319db43cb594b77d6a0e0236
hadron13 <carlosbpfonseca@gmail.com>
2026-04-24 21:17:29 -0300

    Buttload o' fluids

--- src/main/java/io/github/hadron13/petrochem/register/PetrochemFluids.java
+++ src/main/java/io/github/hadron13/petrochem/register/PetrochemFluids.java
@@ -10,6 +10,7 @@ import io.github.hadron13.petrochem.Petrochem;
 import net.createmod.catnip.theme.Color;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.MapColor;
@@ -56,25 +57,30 @@ public class PetrochemFluids {
                     .properties(p -> p.mapColor(MapColor.TERRACOTTA_YELLOW))
                     .build()
                     .bucket()
-//                    .onRegister(AllFluids::registerFluidDispenseBehavior)
                     .tag(AllTags.forgeItemTag("buckets/petroleum")) //TODO: remove this
                     .build()
                     .register();
 
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> RESIN = REGISTRATE
-            .fluid("resin",
-                    Petrochem.asResource("fluid/resin_still"),
-                    Petrochem.asResource("fluid/resin_flow"))
-            .lang("Resin")
-            .properties(p -> p.density(1500).viscosity(2000))
-            .fluidProperties(p -> p.levelDecreasePerBlock(3)
-                    .tickRate(25)
-                    .slopeFindDistance(3)
-                    .explosionResistance(100f))
-            .source(ForgeFlowingFluid.Source::new)
-            .bucket()
-            .tag(AllTags.forgeItemTag("buckets/resin"))
-            .build().register();
+
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> SULFURIC_ACID =
+            REGISTRATE.standardFluid("sulfuric_acid",
+                            SolidRenderedPlaceableFluidType.create(0xd66d842,
+                                    () -> 1f / 32f ))
+                    .lang("Sulfuric Acid")
+                    .properties(b -> b.viscosity(500)
+                            .density(1000))
+                    .fluidProperties(p -> p.levelDecreasePerBlock(1)
+                            .tickRate(25)
+                            .slopeFindDistance(5)
+                            .explosionResistance(100f))
+                    .tag(FluidTags.LAVA)
+                    .source(ForgeFlowingFluid.Source::new)
+                    .block()
+                    .properties(p -> p.mapColor(MapColor.COLOR_YELLOW))
+                    .build()
+                    .bucket()
+                    .build()
+                    .register();
 
 
     public static final FluidEntry<VirtualFluid> AIR = REGISTRATE
@@ -85,25 +91,54 @@ public class PetrochemFluids {
             .build()
             .register();
 
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> NITROGEN = gas("nitrogen");
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> OXYGEN = gas("oxygen");
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> HYDROGEN = gas("hydrogen");
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> ARGON = gas("argon");
 
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> STEAM = gas("steam");
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> AMMONIA = gas("ammonia");
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> CHLORINE = gas("chlorine");
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> DINITROGEN_TETROXIDE = gas("dinitrogen_tetroxide");
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> NITROGEN = gas("Nitrogen");
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> OXYGEN = gas("Oxygen");
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> HYDROGEN = gas("Hydrogen");
+
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> STEAM = gas("Steam");
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> CHLORINE = gas("Chlorine");
     public static final FluidEntry<ForgeFlowingFluid.Flowing> HYDROGEN_SULFIDE = gas("hydrogen_sulfide");
     public static final FluidEntry<ForgeFlowingFluid.Flowing> VOLATILE_GAS = gas("volatile_gas");
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> BUTANE = gas("butane");
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> PROPANE = gas("propane");
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> LPG = gas("lpg");
-    public static final FluidEntry<ForgeFlowingFluid.Flowing> ETHYLENE = gas("ethylene");
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> BUTANE = gas("Butane");
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> PROPANE = gas("Propane");
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> LPG = gas("Lpg");
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> ETHYLENE = gas("Ethylene");
+    //    public static final FluidEntry<ForgeFlowingFluid.Flowing> ARGON = gas("argon");
+//    public static final FluidEntry<ForgeFlowingFluid.Flowing> AMMONIA = gas("ammonia");
+//    public static final FluidEntry<ForgeFlowingFluid.Flowing> DINITROGEN_TETROXIDE = gas("dinitrogen_tetroxide");
+
+
+    public static final FluidEntry<ForgeFlowingFluid.Flowing>
+        OIL_BRINE    = oillike("oil_brine", "Petroleum Brine", 0x373e42),
+        DESALTED_OIL = oillike("desalted_oil", "Desalted Petroleum", 0x482e37),
+        OIL          = oillike("oil", "Oil", 0x11141d),
+        LIGHT_NAPHTA = oillike("light_naphta", "Light Naphta", 0xd9d8a3),
+        HEAVY_NAPHTA = oillike("heavy_naphta", "Heavy Naphta", 0xc4c26e),
+        DESULFURIZED_HEAVY_NAPHTA = oillike("desulfurized_heavy_naphta", "Desulfurized Heavy Naphta", 0xcfc254),
+        PLASTIC = oillike("plastic", "Liquid Polyethylene", 0xd8d8d5),
+        HYDROCRACKED_GASOLINE = oillike("hydrocracked_gasoline", "Raw Gasoline", 0xa68d3f),
+        UNTREATED_GASOLINE = oillike("untreated_gasoline", "Untreated Gasoline", 0xc49b21),
+        GASOLINE = oillike("gasoline", "Refined Gasoline", 0xcfc254),
+        KEROSENE = oillike("kerosene", "Kerosene", 0x26a69a),
+        DESULFURIZED_KEROSENE = oillike("desulfurized_kerosene", "Desulfurized Kerosene", 0x26a69a),
+        LIGHT_DIESEL = oillike("light_diesel", "Light Diesel", 0xb58c4f),
+        HEAVY_DIESEL = oillike("heavy_diesel", "Heavy Diesel", 0x856638),
+        REFINED_DIESEL = oillike("diesel", "Refined Diesel", 0xe57373),
+        LIGHT_GAS_OIL = oillike("light_gas_oil", "Light Gas Oil", 0x5e7a88),
+        HEAVY_GAS_OIL = oillike("heavy_gas_oil", "Heavy Gas Oil", 0x2c393f),
+        HYDROTREATED_GAS_OIL = oillike("hydrotreated_gas_oil", "Hydrotreated Gas Oil", 0x3c394f),
+        DESULFURIZED_HEAVY_DIESEL = oillike("desulfurized_heavy_diesel", "Desulfurized Heavy Diesel", 0xb54f4f),
+        OIL_RESIDUE = oillike("oil_residue", "Oil Residue", 0x311111),
+        HEAVY_OIL_RESIDUE = oillike("heavy_oil_residue", "Heavy Oil Residue", 0x111111),
+        FUEL_OIL =  oillike("fuel_oil", "Fuel Oil", 0x525252),
+        ALKYLATE =  oillike("fuel_oil", "Fuel Oil", 0xb1a7c3),
+        LUBRICANT = oillike("lubricant", "Lubricant", 0xffc107)
+    ;
 
     public static FluidEntry<ForgeFlowingFluid.Flowing> gas(String name){
         return REGISTRATE
-            .fluid(name, Petrochem.asResource("fluid/" + name + "_still"), Petrochem.asResource("fluid/" + name + "_flow"), TransparentFluidType::new)
+            .fluid(name.toLowerCase(), Petrochem.asResource("fluid/" + name.toLowerCase() + "_still"), Petrochem.asResource("fluid/" + name.toLowerCase() + "_flow"), TransparentFluidType::new)
             .properties(p -> p.viscosity(0).density(-100))
             .fluidProperties(p -> p.levelDecreasePerBlock(7)
                     .tickRate(1)
@@ -113,6 +148,28 @@ public class PetrochemFluids {
             .bucket()
             .build()
             .register();
+    }
+
+
+    public static FluidEntry<ForgeFlowingFluid.Flowing> oillike(String name, String lang, int fogColor){
+        return REGISTRATE.standardFluid(name,
+                            SolidRenderedPlaceableFluidType.create(0x352228,
+                                    () -> 1f / 32f ))
+                    .lang(lang)
+                    .properties(b -> b.viscosity(2000)
+                            .density(1000))
+                    .fluidProperties(p -> p.levelDecreasePerBlock(3)
+                            .tickRate(25)
+                            .slopeFindDistance(3)
+                            .explosionResistance(100f))
+                    .source(ForgeFlowingFluid.Source::new)
+                    .block()
+                    .properties(p -> p.mapColor(MapColor.TERRACOTTA_YELLOW))
+                    .build()
+                    .bucket()
+//                    .onRegister(AllFluids::registerFluidDispenseBehavior)
+                    .build()
+                    .register();
     }
 //
 //    public static final ResourceLocation GAS_STILL = new ResourceLocation("minecraft", "block/water_still");
